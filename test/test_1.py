a28979c995b4dd0870c0405c8b83652865e0707e
maharshsheth29-blip <maharshsheth29@gmail.com>
2026-03-25 21:40:48 -0400

    I learning to create questions

---
+++ test/test_1.py
@@ -0,0 +1,7 @@
+
+name = input("enter your name: ")
+age = input("enter your age:")
+favorite_color = input("enter your favorite_color : ")
+print(name)
+print(age)
+print(favorite_color)
---
+++ typecasting.py
@@ -0,0 +1,7 @@
+f = 10.18
+# Convert float into int
+i = int(f)
+print(i)
+print("Enter Your Name: ")
+name = input("enter your name: ")
+print("\nHello", name, "\nwelcome of codingal")
