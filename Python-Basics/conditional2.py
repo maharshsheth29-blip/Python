49725de517e66853742bb7b4b42e3f2191351ec3
maharshsheth29-blip <maharshsheth29@gmail.com>
2026-04-22 21:42:25 -0400

    I leared more conditional based statment

---
+++ Python-Basics/conditional2.py
@@ -0,0 +1,10 @@
+input_1 = int(input("enter a number:"))
+input_2 = int(input("enter a number:"))
+input_3 = (input_1 + input_2)
+input_4 = (input_1 * input_2)
+input_5 = (input_1/input_2)
+
+if input_3 == input_4:
+    print ("input 3 is equal to input 4")
+else: 
+    print ("input 3 is not equal to input 4")
